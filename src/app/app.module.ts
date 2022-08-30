c71fba3fc526d1cbf94fc54152cdb6f16d20f980
Krivobarac <gasparovicsasa@yahoo.com>
2022-08-30 16:32:27 +0200

    ngx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GeneralComponent } from './general/general.component';
+import { NgxsModule } from '@ngxs/store';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { DesignutilityService } from './designutility.service';
+import { AppState } from './states/app.state';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GeneralComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgxsModule.forRoot([AppState]),
+    NgxsLoggerPluginModule,
+    NgxsReduxDevtoolsPluginModule
   ],
-  providers: [],
+  providers: [DesignutilityService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
